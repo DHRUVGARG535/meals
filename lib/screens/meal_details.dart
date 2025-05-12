4b3d2adf9a41f54a5e2dafa40057deff6baabcd1
DHRUVGARG535 <dhruvtbff0811@gmail.com>
2025-05-12 07:48:03 +0530

    favourite meal filter logic moved to provider

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -10,6 +10,9 @@ class MealDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favouriteMeals = ref.watch(favouriteProvider);
+
+    final isFavourite = favouriteMeals.contains(meal);
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -30,7 +33,7 @@ class MealDetailScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(Icons.star),
+            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
