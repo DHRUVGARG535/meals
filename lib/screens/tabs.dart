4149b0fc13e778df0e16159145e36dd4a6797e79
DHRUVGARG535 <dhruvtbff0811@gmail.com>
2025-05-12 07:49:21 +0530

    Import removed

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/provider.dart/favourites_provider.dart';
-import 'package:meals/provider.dart/meals_provider.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
